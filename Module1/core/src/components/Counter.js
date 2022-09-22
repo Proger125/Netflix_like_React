744ca7937fd988399e9628a83efec0aafc6a150a
Aleksandr_Dzyachenka <aleksandr_dzyachenka@epam.com>
2022-09-22 14:09:48 +0300

    Fix mentors comments

--- Module1/core/src/components/Counter.js
+++ Module1/core/src/components/Counter.js
@@ -1,23 +1,15 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
   
-export default class Counter extends Component {
-  shouldComponentUpdate(nextProps) {
-  
-    if (nextProps.value !== this.props.value) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+export default class Counter extends PureComponent {
 
   render() {
-    console.log("Counter 1 is calling");
+    const {onDecrease, onIncrease, value} = this.props
     return (
       <div>
         <h2>Counter:</h2>
-        <button onClick={this.props.onDecrease}>-</button>
-        <h3>{this.props.value}</h3>
-        <button onClick={this.props.onIncrease}>+</button>
+        <button onClick={onDecrease}>-</button>
+        <h3>{value}</h3>
+        <button onClick={onIncrease}>+</button>
       </div>
     );
   }
