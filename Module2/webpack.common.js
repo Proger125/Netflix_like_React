7a73a11d42c2e7b50eabebfa78dc9e72750a11b3
Aleksandr_Dzyachenka <aleksandr_dzyachenka@epam.com>
2022-10-03 14:24:37 +0300

    Add html to app

--- Module2/webpack.common.js
+++ Module2/webpack.common.js
@@ -8,11 +8,34 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Development',
+      template: './public/index.html',
     }),
   ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './dist'),
     clean: true,
   },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        include: path.resolve(__dirname, 'public'),
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules)/, 
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/react'],
+        },
+      },
+    ]},
 };
--- Module2/webpack.dev.js
+++ Module2/webpack.dev.js
@@ -4,9 +4,4 @@ const { merge } = require('webpack-merge');
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
-
-
 });
