f087c7a8cbec2c3e9bee42b18adc6c3adc1175c8
Aleksandr_Dzyachenka <aleksandr_dzyachenka@epam.com>
2022-10-03 13:39:37 +0300

    Change src

---
+++ Module2/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    var style = {
+      color: 'red',
+      fontSize: 50,
+    };
+    return (
+      <div style={style}>
+       Hello World!!!
+      </div>
+    );
+  }
+}
+export default App;
