6f6dd4b5b71dc25e04ee3d287ce56f9aac49c53a
Александр Дьяченко <43385610+Proger125@users.noreply.github.com>
2022-11-25 15:37:03 +0300

    redux clear

--- Module1/core/src/components/HelloReact.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default class HelloReact extends React.Component {
-
-    render() {
-        return React.createElement("h2", null, "It's my first module in React Mentoring Program!")
-    }
-}
--- README.md
+++
@@ -1,1 +0,0 @@
-# React mentoring program repository
